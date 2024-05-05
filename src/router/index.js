28c3b3d9cbc199d812859951b3fb9f628c66d33f
hamdi <hamdi.babdelhafidh@gmail.com>
2024-05-05 17:33:02 +0200

    2eme methode de protect router avec requiresAuth

--- src/router/index.js
+++ src/router/index.js
@@ -1,101 +1,109 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
-import * as Public from "@/views/public"  
-import * as Admin from "@/views/admin"
-import * as Auth from "@/views/auth"
+import * as Public from "@/views/public";
+import * as Admin from "@/views/admin";
+import * as Auth from "@/views/auth";
 
-import {authGuard} from "@/_helpers/auth-guard"
+import { authGuard } from "@/_helpers/auth-guard";
 
 // localStorage.setItem("token","layane")
-
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'public-layout',
+      path: "/",
+      name: "public-layout",
       component: Public.PublicLayout,
-      children:[
+      children: [
         {
-          path: '/',
-          name: 'home',
-          component: Public.Home
+          path: "/",
+          name: "home",
+          component: Public.Home,
         },
         {
-          path: '/about',
-          name: 'about',
-          component: Public.About
+          path: "/about",
+          name: "about",
+          component: Public.About,
         },
         {
-          path: '/contact',
-          name: 'contact',
-          component: Public.Contact
+          path: "/contact",
+          name: "contact",
+          component: Public.Contact,
         },
         {
-          path: '/cocktails',
-          name: 'cocktails',
-          component: Public.Cocktail
+          path: "/cocktails",
+          name: "cocktails",
+          component: Public.Cocktail,
         },
-      ]
+      ],
     },
     {
-      path: '/admin',
-      name: 'admin',
+      path: "/admin",
+      name: "admin",
       // beforeEnter:suthGuard, on letulise pas dans le cas de children "simple route"
       component: Admin.AdminLayout,
-      children:[
+      children: [
         {
-          path: 'dashboard',
-          name: 'Dashboard',
-          component: Admin.Dashboard
+          path: "dashboard",
+          name: "Dashboard",
+          component: Admin.Dashboard,
+          meta: { requiresAuth: true },
         },
         {
-          path: 'users/index',
-          name: 'User index',
-          component: Admin.UserIndex
+          path: "users/index",
+          name: "User index",
+          component: Admin.UserIndex,
         },
         {
-          path: 'users/edit/:id(\\d+)',
-          name: 'User Edit',
-          component: Admin.UserEdit, props:true
+          path: "users/edit/:id(\\d+)",
+          name: "User Edit",
+          component: Admin.UserEdit,
+          props: true,
         },
         {
-          path: 'users/add',
-          name: 'user add',
-          component: Admin.UserAdd
+          path: "users/add",
+          name: "user add",
+          component: Admin.UserAdd,
         },
         {
-          path: 'cocktails/index',
-          name: 'cocktails index',
-          component: Admin.CocktaiIIndex
+          path: "cocktails/index",
+          name: "cocktails index",
+          component: Admin.CocktaiIIndex,
         },
         {
-          path: 'cocktails/edit/:id',
-          name: 'cocktails edit',
-          component: Admin.CocktailEdit
+          path: "cocktails/edit/:id",
+          name: "cocktails edit",
+          component: Admin.CocktailEdit,
         },
         {
-          path: '/:pathMatchall(.*)*',
-          redirect:'/admin/dashboard'
+          path: "/:pathMatchall(.*)*",
+          redirect: "/admin/dashboard",
         },
-      ]
+      ],
     },
     {
-      path: '/login',
-      component: Auth.Login},
-    {
-      path: '/:pathMatchall(.*)*',
-      component: Public.NotFound
+      path: "/login",
+      component: Auth.Login,
     },
-  ]
-})
+    {
+      path: "/:pathMatchall(.*)*",
+      component: Public.NotFound,
+    },
+  ],
+});
 
 router.beforeEach((to, from, next) => {
- if(to.matched[0].name ==="admin"){
-  authGuard()
- }
- next()
-})
+  console.log('====================================');
+  console.log(to.meta.requiresAuth);
+  console.log('====================================');
+  // if (to.matched[0].name === "admin") {
+  //   authGuard();
+  // }
+    if (to.meta.requiresAuth) {
+    authGuard();
+  }
+  next();
+});
 
-export default router
+export default router;
