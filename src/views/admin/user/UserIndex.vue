9592ce14e9e47e43a2e4c4e1478b4671a6ada278
hamdi <hamdi.babdelhafidh@gmail.com>
2024-05-09 21:10:43 +0200

    get lists of users

--- src/views/admin/user/UserIndex.vue
+++ src/views/admin/user/UserIndex.vue
@@ -1,15 +1,58 @@
 <template>
-    <div>
-        Page UserIndex
-    </div>
+  <div>
+    <h3>Page UserIndex (il y en a {{comptage}})</h3>
+    <table>
+        <thead>
+            <tr>
+                <th>Id</th>
+                <th>Nom</th>
+                <th>Prenom</th>
+                <th>Email</th>
+                <th>Creation</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(user, index) in users" :key="user.id">
+            <td>{{ user.id }}</td>
+            <td>{{ user.nom }}</td>
+            <td>{{ user.prenom }}</td>
+            <td>{{ user.email }}</td>
+            <td>{{dateFormat[index] }}</td>
+            </tr>
+        </tbody>
+    </table>
+  </div>
 </template>
 
 <script>
-import {userServices} from "@/_services"
-export default{
-    name:'UserIndex',
-    mounted(){
-        userServices.getAllusers().then(res=> console.log(res.data?.data)).catch(error=>console.log(error))
+import { userServices } from "@/_services";
+export default {
+  name: "UserIndex",
+  data() {
+    return {
+        users: []
     }
-}
+  },
+  mounted() {
+    userServices
+      .getAllusers()
+      .then((res) =>{
+        console.log(res.data.data);
+        this.users = res.data?.data})
+      .catch((error) => console.log(error));
+  },
+  methods:{
+    // dateFormat(date){
+    //   return  date.split("T")[0].split('-').reverse().join("/")
+    // }
+  },
+  computed:{
+    comptage(){
+       return this.users.length
+    },
+    dateFormat(){
+      return  this.users.map(user=> user.createdAt.split("T")[0].split('-').reverse().join("/")) 
+    }
+  }
+};
 </script>
